5dd57a7d9f527d7ddd3788768b6c3d1240ccfd46
quirkles <aquirk@surface-laptop.localdomain>
2018-08-06 02:40:54 -0400

    get port form env

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('dist'))
+app.use(express.static(__dirname + '/dist'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log('Example app listening on port 3000!'))
