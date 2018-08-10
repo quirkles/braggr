50108c2acdee66c289b398248aa215754d6c13b8
quirkles <al.quirk@gmail.com>
2018-08-10 14:30:04 -0400

    Add bg switcher

--- index.jsx
+++ index.jsx
@@ -15,41 +15,74 @@ const copyBrag = brag => {
   	document.body.removeChild(el)
 }
 
+const backgroundClasses = [
+	{
+		className: 'bg-unicorn',
+		displayText: 'Unicorns'
+	},
+	{
+		className: 'bg-space',
+		displayText: 'Space'
+	},
+	{
+		className: 'bg-smiley',
+		displayText: 'Smileys'
+	},
+]
 
 class App extends React.Component {
 	constructor(props) {
     	super(props)
     	this.state = {
-				brag: getBrag()
+				brag: getBrag(),
+				selectedBackgroundClass: backgroundClasses[0]
 			}
     	this.generateBrag = this.generateBrag.bind(this)
-  	}
+	}
 
-  	generateBrag() {
+	generateBrag() {
 		this.setState({
 			brag: getBrag()
 		})
 	}
 
+  setBackgroundClass(backGroundClass) {
+    this.setState({
+      selectedBackgroundClass: backGroundClass
+    })
+  }
+
 	render() {
-		const { brag } = this.state
+		const { brag, selectedBackgroundClass } = this.state
 		return (
-			<div className="App">
-				<input
-					type="text"
-					value={brag}
-					id="bragTextField"
-					style={{display: 'none'}}
-					onChange={() => {}}
-				/>
-				<div className="brag-container">
-					<h1>{ brag }</h1>
-				</div>
-				<div>
-					<div className="button" onClick={this.generateBrag} >Generate new brag</div>
-				</div>
-				<div>
-					<div className="button" onClick={() => copyBrag(brag)} >Copy to cipboard</div>
+			<div className={`App ${selectedBackgroundClass.className}`}>
+				<div className="app-container ">
+					<input
+						type="text"
+						value={brag}
+						id="bragTextField"
+						style={{display: 'none'}}
+						onChange={() => {}}
+					/>
+					<div className="bg-selectors">
+						{backgroundClasses.map(backGroundClass =>
+							<div
+								className={selectedBackgroundClass.className === backGroundClass.className ? 'selected' : ''}
+								onClick={() => this.setBackgroundClass(backGroundClass)}
+							>
+								{backGroundClass.displayText}
+							</div>
+						)}
+					</div>
+					<div className="brag-container">
+						<h1>{ brag }</h1>
+					</div>
+					<div>
+						<div className="button" onClick={this.generateBrag} >Generate new brag</div>
+					</div>
+					<div>
+						<div className="button" onClick={() => copyBrag(brag)} >Copy to cipboard</div>
+					</div>
 				</div>
 			</div>
 		)
